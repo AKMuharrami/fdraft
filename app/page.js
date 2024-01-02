965ca0208f924e89a04a27bd63b6894104388ff7
Asad Al Muharrami <akmuharrami@gmail.com>
2024-01-02 16:55:24 +0400

    llll

--- app/page.js
+++ app/page.js
@@ -2,10 +2,7 @@ import ResponsiveAppBar from './menubar';
 import Homee from "./home";
 export const metadata = {
   title: "Oman (Omani) Web Developer",
-  description: "Experienced In HTML CSS Javascript React, ready to design your proffesional website",
-  alternates: {
-    canonical: '/'
-  }
+  description: "Experienced In HTML CSS Javascript React, ready to design your proffesional website"
 };
 export default function Home() {
       return( <Homee></Homee>)
