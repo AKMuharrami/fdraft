b5323038bad9b68d63ca26500d0e5f8d1213e755
Asad Al Muharrami <akmuharrami@gmail.com>
2024-01-19 11:04:36 +0400

    wwww

--- app/home.js
+++ app/home.js
@@ -446,7 +446,7 @@ export default function Homee() {
 
 
               </motion.div>
-              {/* so how does that link up to our current topix you might say, well let's see */}
+             
               <div id='pbody' style={{marginTop:'-13px'}}>
               <svg id="progress" width="100" height="100" viewBox="0 0 100 100" >
       <circle cx="50" cy="50" r="20" pathLength="1" className="bg" />
